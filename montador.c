205d2a8e2cc1b0975253e7a0dc6bb9156fac65ee
Alexandre Tomy <aledts.gnr@gmail.com>
2015-12-02 19:26:00 -0200

    funcionou! - printa todos as linhas com bytes e mne

--- montador.c
+++ montador.c
@@ -6,10 +6,10 @@
 
 typedef struct{
     int valor;
+    int temRot;
     char rot[10];
     char mn[4];
-    char ope1[4];
-    char ope2[4];
+    char ope[2][4];
 } LINHA;
 
 LINHA linha[100];
@@ -23,125 +23,104 @@ void copy_string(char *target, char *source) {
    *target = '\0';
 }
 
-int Rotulos(FILE *cod){
-    int max=20,l=0,j=0,k=0;
-    int temEQU=0, linhas=0;
-    char *aux;
-    char quebra[]=" \n\t;[],",lin[max],valor[4];
-
-    while(!feof(cod))    {
-        fgets(lin,max,cod); //LE LINHA
-
-        aux=strtok(lin,quebra); //QUEBRA LINHA quando encontrar quebra[]
-        temEQU=0;
-
-        while(aux!= NULL){ //PROCURA EQU
-            if(!strcmp(aux,"EQU")){
-                strcpy(linha[j].rot,lin);
-                aux=strtok(NULL,quebra);
-                printf("\n%s\n",aux);
-                linha[j].valor=atoi(aux);
-                strcpy(linha[j].mn,"xxx");
-                strcpy(linha[j].ope1,"xxx");
-                strcpy(linha[j].ope2,"xxx");
-                temEQU=1;
-                j++;
-                linhas++;
-            }
-
-            aux=strtok(NULL,quebra);
-
-
-        }
-
-        if(!temEQU){ //SE NAO TEM EQU, PROCURA ROTULO
-            aux=strtok(lin,quebra);
-            while(aux!= NULL){
-                if(!strcmp(aux,"MOV")) break;
-                else if(!strcmp(aux,"ADD")) break;
-                else if(!strcmp(aux,"SUB")) break;
-                else if(!strcmp(aux,"CMP")) break;
-                else if(!strcmp(aux,"JMP")) break;
-                else if(!strcmp(aux,"JC")) break;
-                else if(!strcmp(aux,"JNC")) break;
-                else if(!strcmp(aux,"JZ")) break;
-                else if(!strcmp(aux,"JNZ")) break;
-                else if(!strcmp(aux,"JBE")) break;
-                else if(!strcmp(aux,"JA")) break;
-                else if(!strcmp(aux,"CALL")) break;
-                else if(!strcmp(aux,"RET")) break;
-                else if(!strcmp(aux,"HLT")) break;
-                else if(!strcmp(aux,"INC")) break;
-                else{ //ROTULO
-                    strcpy(linha[k+j].rot,aux);
-                    strcpy(linha[k+j].mn,"xxx");
-                    strcpy(linha[k+j].ope1,"xxx");
-                    strcpy(linha[k+j].ope2,"xxx");
-                    linha[k+j].valor=l;
-                    k++;
-                    linhas++;
-                }
-                aux=strtok(NULL,"\n");
-            }
-            l+=2;
-        }
-    }
-    return (linhas);
+int temNaString(char ch, char str[]){
+    int i;
+    for(i=0;i<strlen(str);i++)
+        if (str[i]==ch) return 1;
+    return 0;
 }
 
 int rotulos(FILE *cod){
-    int max=100,l=0,j=0,k=0;
+    int max=100,l=0,j=0,k=0,i=0;
     int com=0;
-    int temEQU=0, bytes=0,rots=0,temROT;
+    int linhaComentada,temQuebra, temB;
+    int temEQU=0, bytes=0,rots=0, totalBytes=0;
     char *aux;
-    char quebra[]=" [],\n;",lin[max],linAux[max];
+    char quebra[]=" ,\n\t",lin[max],linAux[max];
 
-    char mnemonicos[]="/MOV/ADD/SUB/CMP/JMP/JC/JNC/JZ/JNZ/JBE/JA/CALL/RET/HLT/INC/";
+    char mnemonicos[]="/MOV/ADD/SUB/CMP/JMP/JC/JNC/JZ/JNZ/JBE/JA/CALL/RET/HLT/INC/DEC/";
+    char doisBytes[]="/MOV/ADD/SUB/CMP/JMP/JC/JNC/JZ/JNZ/JBE/JA/CALL/";
+    char doisBytesCTZ[]="/JMP/JC/JNC/JZ/JNZ/JBE/JA/CALL/";
+    char umByte[]="/INC/DEC/RET/HLT/";
 
-    //if(strstr(comandos,"/");
     //printf("%s\n",aux);
     while(!feof(cod))    {
         fgets(lin,max,cod); //LE LINHA
 
         aux=strtok(lin,quebra); //QUEBRA LINHA quando encontrar quebra[]
-        //com=0;
+
         while(aux!=NULL){
-            //com++;
-            //
 
             if(!strcmp("EQU",aux)){
+                    linha[l].temRot=1;
                     aux=strtok(NULL," ");
                     //printf("%s\n",aux);
                     linha[l].valor=atoi(aux);
                     break;
             }
 
-            //OPA
-
-
             else if (strstr(mnemonicos,aux)==NULL){
                 strcpy(linha[l].rot,aux);
-                //break;
+                linha[l].temRot=1;
             }
 
             else if (strstr(mnemonicos,aux)!=NULL){
+
+                linha[l].valor=totalBytes;
+
+                linhaComentada=0;
+                temQuebra=0;
+                temB=0;
+                i=0;
+                bytes=0;
+
+
                 strcpy(linha[l].mn,aux);
-
                 aux=strtok(NULL,quebra);
-                printf("%s\n",aux);
 
+                printf("%s",linha[l].mn);
+
+                while(aux!=NULL){
+                    //printf("%s\n",aux);
+
+                    if(strstr(aux,";")!=NULL){
+                        aux[strlen(aux)-1]='\0';
+                        linhaComentada=1;
+                    }
+
+
+                        printf("\t%s\t(%d)",aux,i);
+                        //strcpy(linha[l].ope[i],aux);
+
+                        if (!strcmp("B",aux) || !strcmp("[B]",aux)){
+                            bytes=1;
+                            temB=1;
+                        }
+
+                        if(strstr(umByte,linha[l].mn)!=NULL)
+                            bytes=1;
+
+                        if(strstr(doisBytes,linha[l].mn)!=NULL && !temB)
+                            bytes=2;
+
+                    i++;
+                    if (!linhaComentada){
+                        aux=strtok(NULL,quebra);
+                        //printf("%s %d",aux,i);
+                    }
+                    else break;
+                }
+
+                totalBytes+=bytes;
+                printf(" %d\n",bytes);
 
                 break;
             }
             //printf("%s\n",aux);
 
-
             aux=strtok(NULL,quebra); //quebra por partes ate o final da linha
 
         }
-
-        //printf("%d - %s\n",com,lin);
-        //if (temROT) rots++;
         l++;
     }
     return (l);
@@ -154,7 +133,7 @@ int main(){
     char lin[20];
     char nome[20];
     FILE *ASM, *OPC;
-
+    char quebra[]=" [],;";
     gets(nome);
 
     if ((ASM = fopen(nome,"rt")) == NULL)    {
@@ -169,14 +148,13 @@ int main(){
 
     printf("\n%d\n",linhas=rotulos(ASM));
 
-    printf("\nVALOR\tROTULO\t\tMNE\tOP 1\tOP 2\n");
+    printf("\nVALOR\tROTULO\t\tMNE\tOP 1\t\tOP 2\n");
     for(i=0; i<linhas; i++)
     //for(i=0; i<linhas&&strcmp(linha[i].rot,"\0"); i++)
-        printf("[%d]\t[%s\t]\t[%s\t[%s\t[, %s]]]\n",
+        printf("[%d]\t(%s\t)\t(%s)\t(%s\t)\t(%s\t)\n",
         linha[i].valor, linha[i].rot, linha[i].mn,
-        linha[i].ope1, linha[i].ope2);
+        linha[i].ope[0], linha[i].ope[1]);
 
     fclose(ASM);
     fclose(OPC);
-
 }
